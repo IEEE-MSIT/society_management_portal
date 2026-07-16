6b52effd948b2db4bb5986bfac63bba2438a0429
Gourav <gou4371@gmail.com>
2026-07-16 19:38:42 +0530

    Fix: move prisma to dependencies and align port to 10000

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,7 @@ import collaborationRoutes from './routes/collaborationRoutes.js';
 import { errorHandler } from './middlewares/error.js';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Security Middlewares
 app.use(helmet());
